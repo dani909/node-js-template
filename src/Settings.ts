f5e9fd8e98f29f1c9f3ab6acb632e5d938e33745
dani09 <seppel.h@live.de>
2018-04-15 18:25:23 +0200

    Made loadSettings throw errors

--- src/Settings.ts
+++ src/Settings.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 
+//TODO add other settings for example a folder for logs
 export default class Settings {
     name: string;
     httpPort: number;
@@ -8,18 +9,30 @@ export default class Settings {
     privateKeyPath: string;
     certificatePath: string;
     dbUrl: string;
-    logFolder: string;
 
-    static loadSetting = function (useDefault: boolean, settingName: string, path: string): Settings {
+    static loadSettings = function (useDefault: boolean, settingName: string, path: string): Settings {
         var json = fs.readFileSync(path, 'utf8')
 
         var allSettings = { standartSetting: '', settings: Array<Settings>() }
         allSettings = JSON.parse(json)
 
+        if(allSettings.settings.length == 0) throw "No Settings specified";
+
         if (useDefault) {
-            return allSettings.settings.filter(s => s.name === allSettings.standartSetting)[0]
+            console.log(allSettings.standartSetting)
+            if(allSettings.standartSetting == null) throw 'No Settings arg found and no standartSetting specified'
+
+            var filteredSettings = allSettings.settings.filter(s => s.name === allSettings.standartSetting)
+
+            if(filteredSettings.length == 0) throw "The standartSetting with the Name " + allSettings.standartSetting + " couldn't be found!"
+
+            return filteredSettings[0]
         } else {
-            return allSettings.settings.filter(s => s.name === settingName)[0]
+            var filteredSettings = allSettings.settings.filter(s => s.name === settingName)
+            
+            if(filteredSettings.length == 0) throw "No Settings with the Name " + settingName + " found!"
+
+            return filteredSettings[0]
         }
     }
 };
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log('hello world')
+import Settings from "./Settings";
+import * as path from 'path';
+
+var settings: Settings
+var args = process.argv
+var settingsPath = path.join(__dirname, '..', '/settings.json')
+
+try {
+    if (args.length > 2) {
+        //Specified setting
+        settings = Settings.loadSettings(false, args[2], settingsPath)
+    } else {
+        //Use standart setting
+        settings = Settings.loadSettings(true, '', settingsPath)
+    }
+} catch (e) {
+    console.error('Couldn\'t load Settings:\n' + e)
+    process.exit(1)
+}
+
