574f34b0b7f3433b56605accf806ff69585fd024
dani09 <daniel@dani09.de>
2018-08-16 13:31:55 +0200

    Fixed Docker build on Travis

--- build_docker_ci.sh
+++ build_docker_ci.sh
@@ -1,6 +1,6 @@
 echo "{}" > settings.json
 
-if [ "$IMAGE" = "RPI" ] then
+if [ "$IMAGE" = "RPI" ]; then
     docker run --rm --privileged multiarch/qemu-user-static:register --reset
     docker build . -f RPI.Dockerfile -t node-js-template:latest-RPI
 else
