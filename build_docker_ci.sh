6826ffc299165e4893d99dd065a65e5ce533aaf8
dani09 <daniel@dani09.de>
2018-08-16 13:36:31 +0200

    Removed Tagging in build script at it is not needed

--- build_docker_ci.sh
+++ build_docker_ci.sh
@@ -2,7 +2,7 @@ echo "{}" > settings.json
 
 if [ "$IMAGE" = "RPI" ]; then
     docker run --rm --privileged multiarch/qemu-user-static:register --reset
-    docker build . -f RPI.Dockerfile -t node-js-template:latest-RPI
+    docker build . -f RPI.Dockerfile
 else
-    docker build . -t node-js-template:latest
+    docker build .
 fi
